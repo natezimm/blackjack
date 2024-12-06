fd97606fc6b7ba10abb9b4b6fd98c76eabdf4ab8
nathan <nathan.a.zimmerman@gmail.com>
2024-12-05 23:09:05 -0500

    Add card images

--- client/src/components/PlayerHand.js
+++ client/src/components/PlayerHand.js
@@ -1,14 +1,25 @@
 import React from 'react';
 
 const PlayerHand = ({ hand }) => {
+    const getCardImage = (value, suit) => {
+        // Construct the filename dynamically based on card value and suit
+        const fileName = `${value.toLowerCase()}_of_${suit.toLowerCase()}.png`;
+        return `/card-images/${fileName}`; // Path relative to the public folder
+    };
+
     return (
         <div>
             <h2>Player's Hand</h2>
-            {hand.map((card, index) => (
-                <div key={index}>
-                    {card.value} of {card.suit}
-                </div>
-            ))}
+            <div className="hand">
+                {hand.map((card, index) => (
+                    <div key={index} className="card">
+                        <img
+                            src={getCardImage(card.value, card.suit)}
+                            alt={`${card.value} of ${card.suit}`}
+                        />
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
