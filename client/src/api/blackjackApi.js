4de0b70614d467b91eafd5be2be02722d870ecee
nathan <nathan.a.zimmerman@gmail.com>
2025-12-03 20:52:16 -0500

    Fix hit button

--- client/src/api/blackjackApi.js
+++ client/src/api/blackjackApi.js
@@ -2,18 +2,23 @@ import axios from 'axios';
 
 const API_BASE_URL = process.env.REACT_APP_API_URL;
 
+const apiClient = axios.create({
+    baseURL: API_BASE_URL,
+    withCredentials: true,
+});
+
 export const startGame = async (decks = 1, dealerHitsOnSoft17 = false) => {
-    return axios.get(`${API_BASE_URL}/start`, { params: { decks, dealerHitsOnSoft17 } });
+    return apiClient.get('/start', { params: { decks, dealerHitsOnSoft17 } });
 };
 
 export const hit = async () => {
-    return axios.post(`${API_BASE_URL}/hit`);
+    return apiClient.post('/hit');
 };
 
 export const stand = async () => {
-    return axios.post(`${API_BASE_URL}/stand`);
+    return apiClient.post('/stand');
 };
 
 export const placeBet = async (amount) => {
-    return axios.post(`${API_BASE_URL}/bet`, { amount });
+    return apiClient.post('/bet', { amount });
 };
